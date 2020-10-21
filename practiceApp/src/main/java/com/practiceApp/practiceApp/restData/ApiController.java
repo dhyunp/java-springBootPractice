1108f184f02e9d7870e118d111734a6de5e46294
Donghyun Park <danpark0605@gmail.com>
2020-10-21 20:11:24 +0900

    created post endpoint

--- practiceApp/src/main/java/com/practiceApp/practiceApp/restData/ApiController.java
+++ practiceApp/src/main/java/com/practiceApp/practiceApp/restData/ApiController.java
@@ -3,6 +3,7 @@ package com.practiceApp.practiceApp.restData;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,8 @@ public class ApiController {
                 return new Greeting(counter.incrementAndGet(), String.format(template, name));
         }
 
+        @PostMapping("/helloPost")
+        public String helloPost() {
+                return "{\"result\":\"this is the post endpoint\"}";
+        }
 }
